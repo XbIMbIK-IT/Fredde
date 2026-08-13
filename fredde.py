193db8b06cd3ade30d28af490efcb7d55f8e10c2
AbubachirTrahov <stalkergyz228@gmail.com>
2026-08-13 16:49:34 +0300

    Enhance constructor with gender and visual attributes
    Added gender and visual parameters to the class constructor.

--- fredde.py
+++ fredde.py
@@ -5,23 +5,36 @@ class Fredde:
         self,
         name,
         age=1,
-        color=None,
+        gender='bi', #boy, girl, is (intersex), cf (childfree)
         genid=1,
         gendom=1,
         mutrate=5,
         rarity='common',
         parents=None,
-        generation=0
+        generation=0,
+        # Визуальные параметры
+        color=None,
+        eye='blue',
+        hatAcs='cylinder',
+        faceAcs='none',
+        eyeAcs='none',
+        bodyPattern='basic'
     ):
         self.name = name
         self.age = age
-        self.color = color if color else [255, 0, 0]
+        self.color = color if color else [255, 185, 107]
         self.genid = genid
         self.gendom = gendom
         self.mutrate = mutrate
         self.rarity = rarity
         self.parents = parents if parents else []
         self.generation = generation
+        self.gender = gender
+        self.eye = eye
+        self.hatAcs = hatAcs
+        self.faceAcs = faceAcs
+        self.eyeAcs = eyeAcs
+        self.bodyPattern = bodyPattern
 
         freddis.append(self)
         
