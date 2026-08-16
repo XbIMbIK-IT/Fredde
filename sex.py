67b2de2d2cae2f4cad2c2fb16fda8d2447b411c2
Cesariok <vladizukraini5@gmail.com>
2026-08-16 22:40:32 +0300

    Update sex.py

--- sex.py
+++ sex.py
@@ -1,32 +1,28 @@
 from fredde import fredde
 import random
 
-
+with open("NameList.txt", "r", encoding="utf-8") as f:
+    NameList = f.read().splitlines()
 RarityList = ['common', 'uncommon', 'rare', 'epic', 'mythic', 'legendary']
-
-
 SEX_AGE = 3
 
-def try_sex(parent1, parent2):
-    try:
-        if not parent1.alive or not parent2.alive:
-            return None, "Один из родителей мёртв"
 
-        if parent1.gender == 'cf' or parent2.gender == 'cf':
-            return None, "CF не может размножаться"
+def trySex(parent1, parent2):
+    if not parent1.alive or not parent2.alive:
+        return None, "Один из родителей мёртв"
 
-        if parent1.gender == 'is' or parent2.gender == 'is':
-            pass
-        elif parent1.gender == parent2.gender:
-            return None, "Одинаковый пол"
+    if parent1.gender == 'cf' or parent2.gender == 'cf':
+        return None, "CF не может размножаться"
 
-        if parent1.age < SEX_AGE or parent2.age < SEX_AGE:
-            return None, "Один из родителей слишком молод"
+    if parent1.gender == 'is' or parent2.gender == 'is':
+        pass
+    elif parent1.gender == parent2.gender:
+        return None, "Одинаковый пол"
 
-        return sex(parent1, parent2), "Успешно"
+    if parent1.age < SEX_AGE or parent2.age < SEX_AGE:
+        return None, "Один из родителей слишком молод"
 
-    except Exception as e:
-        return None, f"Ошибка при размножении: {e}"
+    return sex(parent1, parent2), "Успешно"
 
 
 def is_inbreeding(parent1, parent2):
@@ -37,73 +33,68 @@ def is_inbreeding(parent1, parent2):
 
 
 def sex(parent1, parent2):
+    if parent1.generation <= parent2.generation:
+        babygeneration = parent2.generation + 1
+    else:
+        babygeneration = parent1.generation + 1
+
     # Общая доминантность
     total_dom = parent1.gendom + parent2.gendom
-
 
     # Шанс мутации
     MutRate = mut_chance(parent1, parent2, total_dom)
 
-    is_mutant = random.uniform(0, 100) <= MutRate
-
-    if parent1.generation <= parent2.generation:
-        generation = parent2.generation + 1
-    else:
-        generation = parent1.generation + 1
-
-
     # GenID
-    genid = (
-        parent1.genid * parent1.gendom +
-        parent2.genid * parent2.gendom
-    ) / total_dom
-
-    genid *= random.uniform(0.6, 1.4)
-    genid = round(genid)
-
+    babygenid = (
+                        parent1.genid * parent1.gendom +
+                        parent2.genid * parent2.gendom
+                ) / total_dom
 
     # Цвет
-    color = []
+    babycolor = []
 
     for i in range(3):
-        value = (
-            parent1.color[i] * parent1.gendom +
-            parent2.color[i] * parent2.gendom
-        ) / total_dom
+        color = (
+                        parent1.color[i] * parent1.gendom +
+                        parent2.color[i] * parent2.gendom
+                ) / total_dom
 
-        if is_mutant:
-            value *= random.uniform(0.5, 2)
-            value = max(
-                0,
-                min(255, round(value[i]))
-            )
+        babycolor.append(round(color))
 
-        color.append(round(value))
-
-
-    # Доминантность
+    # Наследование gendom
     parent1_chance = parent1.gendom / total_dom
 
     if random.random() <= parent1_chance:
-        gendom = parent1.gendom
+        babygendom = parent1.gendom
     else:
-        gendom = parent2.gendom
-
-    if is_mutant:
-        gendom *= random.uniform(0.6, 1.3)
-        gendom = max(0, min(1, gendom))
-
-    gendom = round(gendom, 3)
-
+        babygendom = parent2.gendom
 
     # Редкость
     if random.random() <= parent1_chance:
-        rarity = parent1.rarity
+        babyrarity = parent1.rarity
     else:
-        rarity = parent2.rarity
+        babyrarity = parent2.rarity
 
-    if is_mutant:
-        rarity_index = RarityList.index(rarity)
+    # Мутация
+    if random.uniform(0, 100) <= MutRate:
+
+        # GenID
+        babygenid *= random.uniform(0.6, 1.4)
+
+        # Цвет
+        for i in range(3):
+            babycolor[i] *= random.uniform(0.5, 2)
+            babycolor[i] = max(
+                0,
+                min(255, round(babycolor[i]))
+            )
+
+        # Доминантность
+        babygendom *= random.uniform(0.6, 1.3)
+        babygendom = max(0, min(1, babygendom))
+
+        # Редкость
+        rarity_index = RarityList.index(babyrarity)
 
         if random.random() < 0.25:
             rarity_index += 1
@@ -117,43 +108,43 @@ def sex(parent1, parent2):
 
         babyrarity = RarityList[rarity_index]
 
-
     # Пол ребенка
     gender_roll = random.uniform(0, 100)
     if gender_roll < 45:
-        gender = 'boy'
+        babygender = 'boy'
     elif gender_roll < 90:
-        gender = 'girl'
+        babygender = 'girl'
     elif gender_roll < 95:
-        gender = 'cf'
+        babygender = 'cf'
     else:
-        gender = 'is'
-
+        babygender = 'is'
 
     # Реснички
-    if gender == 'boy':
+    if babygender == 'boy':
         eyelash = False
-    elif gender == 'girl':
+    elif babygender == 'girl':
         eyelash = True
     elif random.random() <= 0.5:
         eyelash = False
     else:
         eyelash = True
-    
 
+    babygenid = round(babygenid)
+    babygendom = round(babygendom, 3)
+    MutRate = round(MutRate, 1)
     return fredde(
-        color=color,
-        genid=genid,
-        gendom=gendom,
+        name=random.choice(NameList),
+        color=babycolor,
+        genid=babygenid,
+        gendom=babygendom,
         mutrate=MutRate,
-        rarity=rarity,
+        rarity=babyrarity,
         parents=[parent1, parent2],
-        generation=generation,
-        gender=gender,
+        generation=babygeneration,
+        gender=babygender,
+        age=1,
         eyelash=eyelash
     )
-
-
 
 
 def mut_chance(parent1, parent2, total_dom):
@@ -172,7 +163,5 @@ def mut_chance(parent1, parent2, total_dom):
     if parent1.generation != parent2.generation:
         gendif = abs(parent2.generation - parent1.generation)
         MutRate += (gendif * 2)
-
-    MutRate = round(MutRate,1)
 
     return MutRate
