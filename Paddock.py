751ef738f041d3faaf9ee4e77b8d0f3cc346a2f1
ShineOfMoon <kirill89535640822@gmail.com>
2026-08-19 22:33:51 +0300

    Create Paddock.py

---
+++ Paddock.py
@@ -0,0 +1,11 @@
+class Paddock(list):
+    def __init__(self, *freds, *, max_freds=5):
+        self.max_freds = max_freds
+        self.freddies = []
+
+        for fred in freds:
+            self.add(fred)
+
+    def add(self, fred):
+        if Len(self.freddies) == max_freds:
+            raise Error("Загон переполнен")
