8f3e74e9754e36c42403a5e87e8fb7e7b37d21a8
ShineOfMoon <kirill89535640822@gmail.com>
2026-08-19 22:38:43 +0300

    Update Paddock.py

--- Paddock.py
+++ Paddock.py
@@ -1,13 +1,18 @@
-class Paddock(list):
+class Paddock:
     def __init__(self, *freds, *, max_freds=5):
         self.max_freds = max_freds
         self.freddies = []
 
         for fred in freds:
-            self._add(fred)
+            self.add(fred)
 
-    def _add(self, fred):
+    def add(self, fred):
         if Len(self.freddies) == max_freds:
             raise Error("Загон переполнен")
         else:
             self.freddies.append(fred)
+
+    def step(self):
+        for fred in self.freddies:
+            if fred.check_death():
+                age += 1
